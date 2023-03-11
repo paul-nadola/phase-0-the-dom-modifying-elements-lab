621091d148421134e7664ee96836e7f44074fc9d
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-11 21:54:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1")
+/*document.body.append(newHeader)*/
+newHeader.id = 'victory'
+newHeader.textContent = 'NADOLA is the champion'
+/*document.body.append(newHeader)*/
